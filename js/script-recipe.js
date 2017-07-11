e3baf7e44f4652e846d965a995e0be41bede4e09
RangiBecerra <rangibecerra@gmail.com>
2017-07-10 22:22:07 -0400

    dar funcionalidad a botones RECIPE MAKEIT animando contenedor principal de la nav

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,4 +3,15 @@ $(document).ready( function(){
 	console.log("recipe.html esta listo");
 	// etapa 1
 	$(".js-menu").hide();
+	// etapa 8
+	$(".js-show-recipe").click(function(){
+		alert("recipe");
+		$(".page.recipe").removeClass("make")
+	});
+	$(".js-show-make").click(function(){
+		alert("make");
+		//.btns-group
+		$(".page.recipe").addClass("make");
+	});
+
 });
