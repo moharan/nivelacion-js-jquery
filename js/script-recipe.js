22aa08e1cc2204a5450d178c0b3da93b291f448e
RangiBecerra <rangibecerra@gmail.com>
2017-07-10 23:10:14 -0400

    etapa: 9 funcionalidad de botones RECIPE MAKEIT para navegacion active

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -5,13 +5,23 @@ $(document).ready( function(){
 	$(".js-menu").hide();
 	// etapa 8
 	$(".js-show-recipe").click(function(){
-		alert("recipe");
-		$(".page.recipe").removeClass("make")
+		//alert("recipe");
+		$(".page.recipe").removeClass("make");
+		// etapa 9
+		$(".js-show-recipe").addClass("active");
+		$(".js-show-recipe").addClass("activo");
+		$(".js-show-make").removeClass("active");
+		$(".js-show-make").removeClass("activo");
 	});
 	$(".js-show-make").click(function(){
-		alert("make");
+		//alert("make");
 		//.btns-group
 		$(".page.recipe").addClass("make");
+		// etapa 9
+		$(".js-show-make").addClass("active");
+		$(".js-show-make").addClass("activo");
+		$(".js-show-recipe").removeClass("active");
+		$(".js-show-recipe").removeClass("activo");
 	});
 
 });
